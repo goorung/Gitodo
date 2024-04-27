e32364465878c2bb91894b21fc9b44d2961ffee4
noeyiz <lamoureclatant@gmail.com>
2024-04-27 13:32:17 +0900

    ✨ feat: 레포지토리 관련 뷰컨트롤러 이동 로직 구현

---
+++ Gitodo/Sources/RepositoryInfoView.swift
@@ -0,0 +1,12 @@
+//
+//  RepositoryInfoView.swift
+//  Gitodo
+//
+//  Created by jiyeon on 4/27/24.
+//
+
+import UIKit
+
+class RepositoryInfoView: UIView {
+    
+}
---
+++ Gitodo/Sources/RepositorySettingsView.swift
@@ -0,0 +1,12 @@
+//
+//  RepositorySettingsView.swift
+//  Gitodo
+//
+//  Created by jiyeon on 4/27/24.
+//
+
+import UIKit
+
+class RepositorySettingsView: UIView {
+    
+}
