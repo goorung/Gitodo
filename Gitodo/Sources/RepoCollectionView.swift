75be333ad160ed3465d83d8c90297d356e058e09
JH713 <strider13@snu.ac.kr>
2024-05-05 16:43:25 +0900

    :sparkles: feat: repository 이동 기능

--- Gitodo/Sources/RepoCollectionView.swift
+++ Gitodo/Sources/RepoCollectionView.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class RepoCollectionView: UICollectionView {
     
-    let tempRepo = [
-        (name: "algorithm", color: UIColor.systemMint, symbol: "☃️"),
-        (name: "iOS", color: UIColor.systemGray, symbol: "☁️"),
+    var tempRepo = [
+        (name: "algorithm", color: .systemMint, symbol: "☃️"),
+        (name: "iOS", color: .systemBrown, symbol: "☁️"),
+        (name: "Gitodo", color: .systemTeal, symbol: "🌝"),
+        (name: "Inception", color: UIColor.systemGray, symbol: "💕"),
+        (name: "algorithm", color: .systemMint, symbol: "☃️"),
+        (name: "iOS", color: .systemGray, symbol: "☁️"),
+        (name: "Gitodo", color: .systemTeal, symbol: "🌝"),
+        (name: "Inception", color: UIColor.systemGray, symbol: "💕"),
     ]
     
     init() {
@@ -28,6 +34,10 @@ class RepoCollectionView: UICollectionView {
         backgroundColor = .clear
         clipsToBounds = true
         dataSource = self
+        delegate = self
+        dragDelegate = self
+        dropDelegate = self
+        dragInteractionEnabled = true
         register(RepositoryInfoCell.self, forCellWithReuseIdentifier: RepositoryInfoCell.reuseIdentifier)
     }
     
@@ -47,6 +57,70 @@ extension RepoCollectionView: UICollectionViewDataSource {
         let repo = tempRepo[indexPath.row]
         cell.configure(name: repo.name, color: repo.color, symbol: repo.symbol)
         return cell
+    }
+    
+}
+
+extension RepoCollectionView: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, canMoveItemAt indexPath: IndexPath) -> Bool {
+        true
+    }
+}
+
+extension RepoCollectionView: UICollectionViewDragDelegate {
+    func collectionView(_ collectionView: UICollectionView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+        [UIDragItem(itemProvider: NSItemProvider())]
+    }
+}
+
+extension RepoCollectionView: UICollectionViewDropDelegate {
+    func collectionView(_ collectionView: UICollectionView, performDropWith coordinator: UICollectionViewDropCoordinator) {
+        var destinationIndexPath: IndexPath
+        if let indexPath = coordinator.destinationIndexPath {
+            destinationIndexPath = indexPath
+        } else {
+            let row = collectionView.numberOfItems(inSection: 0)
+            destinationIndexPath = IndexPath(item: row - 1, section: 0)
+        }
+        
+        guard coordinator.proposal.operation == .move else { return }
+        move(coordinator: coordinator, destinationIndexPath: destinationIndexPath, collectionView: collectionView)
+    }
+    
+    private func move(coordinator: UICollectionViewDropCoordinator, destinationIndexPath: IndexPath, collectionView: UICollectionView) {
+        guard let sourceItem = coordinator.items.first,
+              let sourceIndexPath = sourceItem.sourceIndexPath
+        else { return }
+        
+        performBatchUpdates { [weak self] in
+            self?.move(sourceIndexPath: sourceIndexPath, destinationIndexPath: destinationIndexPath)
+        } completion: { finish in
+            coordinator.drop(sourceItem.dragItem, toItemAt: destinationIndexPath)
+        }
+        
+    }
+    
+    private func move(sourceIndexPath: IndexPath, destinationIndexPath: IndexPath) {
+        let sourceItem = tempRepo[sourceIndexPath.item]
+        
+        DispatchQueue.main.async {
+            self.tempRepo.remove(at: sourceIndexPath.item)
+            self.tempRepo.insert(sourceItem, at: destinationIndexPath.item)
+            let indexPaths = self.tempRepo
+                .enumerated()
+                .map(\.offset)
+                .map{ IndexPath(row: $0, section: 0) }
+            UIView.performWithoutAnimation {
+                self.reloadItems(at: indexPaths)
+            }
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, dropSessionDidUpdate session: UIDropSession, withDestinationIndexPath destinationIndexPath: IndexPath?) -> UICollectionViewDropProposal {
+        guard hasActiveDrag else {
+            return UICollectionViewDropProposal(operation: .forbidden)
+        }
+        return UICollectionViewDropProposal(operation: .move, intent: .insertAtDestinationIndexPath)
     }
     
 }
