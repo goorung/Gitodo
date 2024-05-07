53a1da0dfb38c8ea3320d5db0ba51e24f8018be8
noeyiz <lamoureclatant@gmail.com>
2024-05-07 17:04:17 +0900

    :fire: remove: 예시 코드 삭제

--- Gitodo/Sources/Main/MainViewController.swift
+++ Gitodo/Sources/Main/MainViewController.swift
@@ -17,30 +17,6 @@ class MainViewController: BaseViewController<MainView>, BaseViewControllerProtoc
         setupNavigationBar()
         hideKeyboardWhenTappedAround()
         contentView.setIssueDelegate(self)
-        
-        Task { // 예시 코드
-            do {
-                let organizations = try await APIManager.shared.fetchOrganization()
-                print("\n[ organization list ]")
-                for organization in organizations {
-                    print(organization)
-                }
-                
-                print("\n[ repository list ]")
-                let repositories = try await APIManager.shared.fetchRepositories()
-                for repository in repositories {
-                    print(repository)
-                }
-                
-                print("\n[ issue list ]")
-                let issues = try await APIManager.shared.fetchIssues(for: repositories[1])
-                for issue in issues {
-                    print(issue)
-                }
-            } catch {
-                print("실패")
-            }
-        }
     }
     
     func setupNavigationBar() {
@@ -55,7 +31,6 @@ class MainViewController: BaseViewController<MainView>, BaseViewControllerProtoc
                 print("실패: \(error.localizedDescription)")
             }
         }
-        setProfileImageView(url: nil)
         setProfileImageViewAction(#selector(handleProfileImageViewTap))
     }
     
