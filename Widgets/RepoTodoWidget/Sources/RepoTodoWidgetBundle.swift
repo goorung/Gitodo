fa77622cde6e6b0703083c9d607898a6cecc875d
noeyiz <lamoureclatant@gmail.com>
2024-05-21 19:19:23 +0900

    :ringed_planet: build: Widget 타겟 추가

---
+++ Widgets/RepoTodoWidget/Sources/RepoTodoWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  RepoTodoWidgetBundle.swift
+//  RepoTodoWidget
+//
+//  Created by jiyeon on 5/21/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct RepoTodoWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        RepoTodoWidget()
+    }
+}
